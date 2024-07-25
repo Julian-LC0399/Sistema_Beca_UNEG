442a743b1beb541967a8048d7f181fb058a76115
Julián <julianlopezcastillo12@gmail.com>
2024-07-24 23:27:57 -0400

    S

--- app/Models/Stu_career.php
+++ app/Models/Stu_career.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Stu_career extends Model
 {
     use HasFactory;
-    protected $fillable = ["Student_id", "Career_id"];
+    protected $guarded = ["Student_id ", "Carers_id"];
 }
