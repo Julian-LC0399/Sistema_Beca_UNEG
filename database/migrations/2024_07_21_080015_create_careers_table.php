9b922eaf0ee228b25dc0a734f8dff386354d9de6
Julián <julianlopezcastillo12@gmail.com>
2024-08-14 18:57:49 -0400

    l

--- database/migrations/2024_07_21_080015_create_careers_table.php
+++ database/migrations/2024_07_21_080015_create_careers_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('careers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('Institution_id')->constrained();
+            $table->bigInteger('Institution_id')->unsigned();
+            $table->foreign('Institution_id')->references('id')->on('institutions')->onDelete("cascade");
             $table->string('Name');
             $table->timestamps();
         });
