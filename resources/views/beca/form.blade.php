9759843655accd36a2e701fcbc3fbff481d25071
Julián <julianlopezcastillo12@gmail.com>
2024-08-14 21:18:59 -0400

    s

--- resources/views/beca/form.blade.php
+++ resources/views/beca/form.blade.php
@@ -1,6 +1,6 @@
 <div class="row padding-1 p-1">
     <div class="col-md-12">
-        
+
         <div class="form-group mb-2 mb20">
             <label for="institution_id" class="form-label">{{ __('Institution Id') }}</label>
             <input type="text" name="Institution_id" class="form-control @error('Institution_id') is-invalid @enderror" value="{{ old('Institution_id', $beca?->Institution_id) }}" id="institution_id" placeholder="Institution Id">
@@ -14,6 +14,6 @@
 
     </div>
     <div class="col-md-12 mt20 mt-2">
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+        <button type="submit" class="btn btn-primary">{{ __('Registrar') }}</button>
     </div>
 </div>
