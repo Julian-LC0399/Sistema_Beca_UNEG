21a2aa9f4de9b13a4bf23e4e968f7cb420844e5f
Julián <julianlopezcastillo12@gmail.com>
2024-08-15 11:49:04 -0400

    w

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,13 +64,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Inicio de sesión') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                                 </li>
                             @endif
                         @else
