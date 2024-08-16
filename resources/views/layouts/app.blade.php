1f2dd26deaae03995ecc3677629f71373b3855dc
Julián <julianlopezcastillo12@gmail.com>
2024-08-16 16:02:36 -0400

    sy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Sistema de becas uneg') }}
-                    Sistema de becas uneg
+                    inicio
                 </a>
 
                 <a class="navbar-brand" href="{{ url('/institutions') }}">
