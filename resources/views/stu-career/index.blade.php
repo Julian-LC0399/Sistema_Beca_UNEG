a164e544aed98df3308f85ba7d2fd3a08b8e108e
Julián <julianlopezcastillo12@gmail.com>
2024-08-19 16:35:59 -0400

    d

--- resources/views/stu-career/index.blade.php
+++ resources/views/stu-career/index.blade.php
@@ -13,7 +13,7 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Stu Careers') }}
+                                {{ __('Estudiantes y carrera') }}
                             </span>
 
                              <div class="float-right">
